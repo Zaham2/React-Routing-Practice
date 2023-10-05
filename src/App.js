70fd2924b46b0cc0681c40612dda01d26d499296
zaham <zaham2@yahoo.com>
2023-10-05 04:44:43 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Outlet, useLocation, MemoryRouter, Navigate, BrowserRouter, Link, useNavigate } from 'react-router-dom';
+
 
 function App() {
+  const navigate = useNavigate()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <h1>Hello React</h1>
+      <Routes>
+        <Route path='one' element={<Main text='1' />} />
+        <Route path='three' element={<Button text='1'/>} />
+        <Route path='two/*'>
+          <Route index element={<Main text='2' />} />
+          <Route path='four/*' element={<><Filler text='f'/> <Outlet /></>} />
+          <Route path='nine' element={<Main text='9' />} />
+          <Route path='*' element={<NotFound text={'not found 1'}/>} />
+        </Route>
+      </Routes>
+    </Router>
+  )
+}
+const Button = (props) => {
+  return(
+    <button
+      onClick={() => {
+        console.log('i have been pushed!!');
+      }}
+    >click me! {props.text}</button>
+  )
+}
+
+const NotFound = (props) => {
+  return(
+    <h1>404 not found {props.text}</h1>
+  )
+}
+
+const Main = (props) => {
+  return (
+    <div>
+      <h2>Main Component {props.text}</h2>
     </div>
-  );
+  )
+}
+
+const Dummy = (props) => {
+  return (
+    <div>
+      <h2>Dummy {props.text}</h2>
+    </div>
+  )
+}
+
+const InnerNavigate = () => {
+  return (
+    <div>
+      I am in InnerNavigate now <br />
+      I will print {useLocation().pathname}
+      {/* <Navigate to='seven' replace={true}/> */}
+    </div>
+  )
+}
+
+
+
+
+const Filler = (props) => {
+  console.log(useLocation());
+  return (
+    <div>
+      <h3>Filler {props.text}</h3>
+      <Dummy text='d3' />
+      <Main text='m5' />
+        <MemoryRouter basename='/two'>
+          <Routes>
+            {console.log('zebala1')}
+            <Route index element={<InnerNavigate text={'main 4'}/>} />
+            {console.log('zebala2')}
+            <Route path='/seven' element={<Main text='main 2' />}/>
+            {console.log('zebala3')}
+            <Route path="*" element={<NotFound text='not found 3' />} />
+            {console.log('zebala4')}
+          </Routes>
+        </MemoryRouter>
+    </div>
+  )
 }
 
 export default App;
